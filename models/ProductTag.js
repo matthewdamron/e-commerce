8f21578b51868852b38c7bd5fc6dc32ffdc25a02
Matthew Damron <mattdamron@msn.com>
2021-02-11 20:33:12 -0700

    ability to connect to db schema and seed

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,8 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
-const Product = require('./Product');
-const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
@@ -18,14 +16,14 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: Product,
+        model: 'product',
         key: 'id'
       }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: Tag,
+        model: 'tag',
         key: 'id'
       }
     }
